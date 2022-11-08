b2a1b26adc6b2f0d8d9e12aefd9beb3b4b715e20
Cho <mcho1@babson.edu>
2022-11-08 10:29:17 -0500

    mbta

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-"""
-Simple "Hello, World" application using Flask
-"""
-
-from flask import Flask
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+
+@app.route("/mbta_helper", methods= ["GET", "POST"]) 
+def nearest():
+    """
+    Returns nearest MBTA stop and wheelchair accessibility 
+    """
+    if request.method == "POST":
+        place = request.form["place"]
+        stop, wheelchair_boarding = find_stop_near(place)
+
+        return render_template(
+            "results.html", stop=stop, wheelchair_boarding=wheelchair_boarding
+        )
+    else:
+        return render_template ("index.html", error=True)
+
 
 
 if __name__ == '__main__':
